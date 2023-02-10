04053d8f802bc18d52eb16828d56e9cb631d0a04
James Stuart Clulow <JamesStuart.Clulow@rdor.nestle.com>
2023-02-10 16:47:29 +0100

    update to unit testing

--- tests/testthat/test_f_simluate_continuous_data.R
+++ tests/testthat/test_f_simluate_continuous_data.R
@@ -1,21 +1,21 @@
 
-test_that("For invalid input arguments 'getSimulatedTwoArmMeans' throws meaningful exceptions", { # nolint
+test_that("For invalid input arguments 'get_simulated_two_arm_means' throws meaningful exceptions", { # nolint
     expect_error(
-        getSimulatedTwoArmMeans(),
+        get_simulated_two_arm_means(),
         'argument "n1" is missing, with no default'
     )
 
     # TODO implement tests for all arguments
 })
 
-test_that("With defined seed 'getSimulatedTwoArmMeans' returns reproducible results", { # nolint
+test_that("With defined seed 'get_simulated_two_arm_means' returns reproducible results", { # nolint
     # TODO implement tests
 })
 
-test_that("With undefined seed 'getSimulatedTwoArmMeans' returns valid results", { # nolint
+test_that("With undefined seed 'get_simulated_two_arm_means' returns valid results", { # nolint
     # TODO implement tests
 })
 
-test_that("'getSimulatedTwoArmMeans' result can be printed", {
+test_that("'get_simulated_two_arm_means' result can be printed", {
     # TODO implement tests
 })
